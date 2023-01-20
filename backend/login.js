a94b348eee7a7a5a71e3be2c6c843a1bdb72a57f
Mokshad43 <122999507+Mokshad43@users.noreply.github.com>
2023-01-20 15:01:40 +0530

    Update login.js

--- backend/login.js
+++ backend/login.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const port = 5500;
 
-app.get("/", (req, res){
+app.get("/", (req, res)=>{
     res.send("Welcome to the page");
 })
+
+app.listen(port , () => {
+    console.log(listening to the port at ${port})
+})
+
   
 
