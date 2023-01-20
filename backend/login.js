e3c0040ad43b9d36032fd5d67a1886b924825889
Mokshad43 <mokshad.nemade221@vit.edu>
2023-01-21 01:09:45 +0530

    contactus

--- backend/login.js
+++ backend/login.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 require("./db/conn");
-
+const port  = process.env.PORT || 3000;
 app.get("/", (req, res)=>{
     res.send("Welcome to the page");
-})
+});
 
+app.listen(port,() => {
+    console.log('server is running at port no ');
+});
 
 
 
